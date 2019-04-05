93a820ea1734dcc5caf3f67adc8a9ce93cbd8239
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-05 18:51:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const returnFirstTwoDrivers = function (drivers){ return drivers.slice(0, 2)}
 const returnLastTwoDrivers = function (drivers){ return drivers.slice(drivers.length-2)}
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 const createFareMultiplier = function (value){
-  return function (fare) {return fare * value}
+  return function (fare) {return fare * value};
 }
