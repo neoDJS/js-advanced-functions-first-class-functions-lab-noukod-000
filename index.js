1bc76a750aef7f70685a51e01d226c867f7c7c90
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-05 18:43:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function (drivers){ return drivers.slice(0,2)}
+const returnFirstTwoDrivers = function (drivers){ return drivers.slice(0, 2)}
